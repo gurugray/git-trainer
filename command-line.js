11ddaad51752319149cdc3be26586c7f72267548
Сергей Сергеев <gurugray@yandex-team.ru>
2013-05-13 17:11:35 +0300

    command-line: indention
    convert tabs to spaces

--- command-line.js
+++ command-line.js
@@ -1,9 +1,9 @@
 function CommandLine(input) {
-	var history = [],
-		currentPosition = history.length,
-		input = $(input);
+    var history = [],
+        currentPosition = history.length,
+        input = $(input);
 
-	input.bind('keydown', function(e) {
+    input.bind('keydown', function(e) {
         if (e.keyCode == 13 && input.val() != '') {
             (input.val() != history[history.length-1]) && history.push(input.val());
             input.val('');
@@ -15,16 +15,16 @@ function CommandLine(input) {
             input.val(history[currentPosition]);
         } else if (e.keyCode == 40){
             (currentPosition < history.length) && currentPosition++;
-	        input.val(history[currentPosition]);
+            input.val(history[currentPosition]);
         } else if (e.keyCode == 27) {
             input.val('');
             currentPosition = history.length;
         }
     });
 
-	return {
-		getCurrent: function(){
-			return history[currentPosition-1] ? history[currentPosition-1] : '';
-		}
-	}
+    return {
+        getCurrent: function(){
+            return history[currentPosition-1] ? history[currentPosition-1] : '';
+        }
+    }
 }
