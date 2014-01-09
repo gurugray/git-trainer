4a1187d0bde965c7aa886d7e8dd1e8f7cbe35acc
Сергей Сергеев <gurugray@yandex-team.ru>
2014-01-09 17:39:24 +0200

    remove unneeded marker on creating node
    marker allready added on next tick

--- src/graph.js
+++ src/graph.js
@@ -80,8 +80,7 @@ function Graph(holder, w, h) {
                     .attr('x1', function(d) { return d.source.x; })
                     .attr('y1', function(d) { return d.source.y; })
                     .attr('x2', function(d) { return d.target.x; })
-                    .attr('y2', function(d) { return d.target.y; })
-                    .attr('marker-end', function() { return 'url(#live)'; });
+                    .attr('y2', function(d) { return d.target.y; });
 
         node.enter()
             .append('g')
