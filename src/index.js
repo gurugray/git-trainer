68af749fefd0a33e903159fe388a50c1a17be640
Сергей Сергеев <gurugray@yandex-team.ru>
2014-01-25 13:02:39 +0200

    index: move js code to js file

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@
 /*borschik:include:graph.js*/
 /*borschik:include:command-line.js*/
 /*borschik:include:git-commander.js*/
+
+/* global Graph, Repo, CommandLine, GitCommander */
+
+var myRepo = new Repo(),
+    myGraph = new Graph('.demo', window.innerWidth, window.innerHeight-100),
+    myCommander = new GitCommander(myRepo),
+
+commandLine = new CommandLine('.command-line', function() {
+    myCommander.run(commandLine.getCurrent());
+    myGraph.dataUpdate(myRepo.getData());
+});
+
+document.querySelectorAll('.demo')[0].addEventListener('dblclick', function(e) {
+    if (e.srcElement && e.srcElement.tagName === 'circle') {
+        commandLine.append(e.srcElement.parentNode.dataset.oid);
+    }
+});
