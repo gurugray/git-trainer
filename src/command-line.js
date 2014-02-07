4992dd13afa1a64d0f2d4025e91f197ab704f810
Евгений Мищенко <evgenjion@yandex-team.ru>
2014-02-07 20:02:24 +0200

    Change Command-Line history mechanism

--- src/command-line.js
+++ src/command-line.js
@@ -1,46 +1,64 @@
 /* exported CommandLine */
 function CommandLine(inputSelector, callback) {
 
-    var history = [],
-        currentPosition = history.length,
-        input = document.querySelectorAll(inputSelector)[0];
+    function History(input) {
+        var commands = [],
+            current = 0;
+
+        return {
+
+            prev: function() {
+                (current > 0) && current--;
+                this.update();
+            },
+
+            update: function() { // update current state in input
+                input.value = commands[current];
+            },
+
+            next: function() {
+                (current < commands.length-1) && current++;
+                this.update();
+            },
+            current: function() {
+                return commands[current - 1];
+            },
+
+            clearCurrent: function() {
+                input.value = '';
+                current = commands.length;
+            },
+
+            push: function(command) {
+                if (!command) return;
+                (command !== commands[commands.length - 1]) && commands.push(command);
+                this.clearCurrent();
+            }
+
+        };
+    }
+
+    var input = document.querySelectorAll(inputSelector)[0],
+        history = new History(input);
 
     input.addEventListener('keydown', function(e) {
 
         switch (e.keyCode) {
-            case 13:
-                if (input.value !== '') {
-                    (input.value !== history[history.length - 1]) && history.push(input.value);
-                    input.value = '';
-                    currentPosition = history.length;
-
-                    callback();
-                }
+            case 13: // enter
+                history.push(input.value);
+                callback(history.current());
             break;
 
-            case 38:
-                (currentPosition > 0) && currentPosition--;
-                input.value = history[currentPosition];
-            break;
+            case 38: history.prev(); break; // top arrow
 
-            case 40:
-                (currentPosition < history.length-1) && currentPosition++;
-                input.value = history[currentPosition];
-            break;
+            case 40: history.next(); break; // bottom arrow
 
-            case 27:
-                input.value = '';
-                currentPosition = history.length;
-            break;
+            case 27: history.clearCurrent(); break; //esc
         }
 
     });
 
     return {
-        getCurrent: function() {
-            return history[currentPosition - 1] || '';
-        },
-
         append: function (val) {
             input.value = input.value + val;
             input.focus();
