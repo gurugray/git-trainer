a7af4f3fe9236191539b34c340b309ca36c9496c
Сергей Сергеев <gurugray@yandex-team.ru>
2014-02-27 19:16:20 +0200

    atch: add css files

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
 
         watch: {
             scripts: {
-                files: ['src/*.js'],
+                files: ['src/*.js', 'src/*.css'],
                 tasks: ['lint', 'build'],
                 options: {
                     spawn: false
